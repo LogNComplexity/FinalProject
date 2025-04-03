dcf30808d4d5182b8932c3c377bacc490d468798
LogNComplexitygit config --global user.name LogNComplexity <invisible.shadow19@gmail.com>
2025-04-03 14:52:48 -0400

    fixed up bugs in heuristic function and class graph

--- Finalproject.py
+++ Finalproject.py
@@ -2,7 +2,17 @@ import heapq
 import math
 from math import radians, cos, sin, asin, sqrt
 
-def A_Star(graph, start, goal, heuristic):
+# below is code template for part 2
+
+
+
+
+
+
+
+# below is code template for part 4
+# part 4 A*
+def A_Star_part4(graph, start, goal, heuristic):
     """
     Implements the A* algorithm to find the shortest path from start to goal.
     
@@ -64,8 +74,61 @@ def A_Star(graph, start, goal, heuristic):
     return cameFrom, ([], float('inf'))  # No path found
 
 
-
 # below is code template for part 5
+# part 5 A*
+def A_Star(graph, start, goal, heuristic):
+    
+    # The set of discovered nodes that may need to be (re-)expanded.
+    openSet = [(heuristic[start], start)]  # Priority queue (min-heap)
+    
+    # For node n, cameFrom[n] is the node immediately preceding it on the cheapest path.
+    cameFrom = {}
+    
+    # gScore stores the cost from start to each node, defaulting to infinity.
+    gScore = {node: float('inf') for node in graph.get_graph()}
+    gScore[start] = 0
+    
+    # fScore[n] = gScore[n] + h(n), representing our best guess at total cost.
+    fScore = {node: float('inf') for node in graph.get_graph()}
+    fScore[start] = heuristic[start]
+
+
+    #graph adjacency lists and weights
+    neighbours = graph.get_graph()
+    
+        
+    while openSet:
+        # Get the node with the lowest fScore value
+        _, current = heapq.heappop(openSet)
+        
+        
+        if current == goal:
+            
+            path = []
+            while current in cameFrom:
+                
+                path.append(current)
+                current = cameFrom[current]
+            path.append(start)
+            path.reverse()
+            
+            return (path, gScore[goal])
+        
+        for neighbor in neighbours[current]:
+            
+            
+            weight = graph.get_weight(current,neighbor)
+            tentative_gScore = gScore[current] + weight
+            
+            if tentative_gScore < gScore[neighbor]:
+                # This path to neighbor is better than any previous one.
+                cameFrom[neighbor] = current
+                gScore[neighbor] = tentative_gScore
+                fScore[neighbor] = tentative_gScore + heuristic[neighbor]
+                heapq.heappush(openSet, (fScore[neighbor], neighbor))
+    
+    return ([], float('inf'))  # No path found
+
 class DirectedWeightedGraph:
 
     def __init__(self):
@@ -171,7 +234,7 @@ def experiment():
     heuristic = heuristic_calulater(graph, '163')
     
     return A_Star(graph, '11', '163', heuristic)  # Return the result
-
+print("s")
 print(experiment())  
 
 
